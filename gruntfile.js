6e95828955b15deb559d4b6f0abf7997996098ca
Markus Danek <markus.danek@gmail.com>
2015-07-12 17:26:34 +0200

    added less, jshint, uglify and watch (not working properly)

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,94 @@
-'use strict'
-
-var path = require('path');
-
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    grunt.initConfig({
-        app: {
-            app: 'app'
+    folders: {
+      'dist': 'dist',
+      'app': 'app'
+    },
+
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish'),
+        ignores: [
+        'Grunfile.js', 
+        'app/lib/**/*'
+        ]
+      },
+      all: [
+      'app/**/*.js',
+      ]
+    },
+
+    // configure uglify to minify js files -------------------------------------
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/js/app.min.js': 'app/js/main.js'
+        }
+      }
+    },
+
+    // compile less stylesheets to css -----------------------------------------
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
         },
-        express: {
-            dev: {
-                options: {
-                    port: 9000,
-                    hostname: '0.0.0.0',
-                    bases: ['<%= app.app %>'],
-                    livereload: true
-                }
-            }
-          }
+        files: [
+        {
+          expand: true,
+          cwd: 'app/less',
+          src: ['*.less'],
+          dest: 'app/css/',
+          ext: '.css'
+        }
+        ]
+      }
+    },
 
-    });
+    // starting an express server ----------------------------------------------
+    express: {
+      dev: {
+        options: {
+          port: 9000,
+          hostname: '0.0.0.0',
+          bases: ['app'],
+          livereload: true
+        }
+      }
+    },
 
-    grunt.registerTask('default', ['express', 'express-keepalive']);
+    // configure watch to auto update ------------------------------------------
+    watch: {
+      styles: {
+        files: ['less/**/*.less'],
+        tasks: ['less', 'cssmin'],
+        options: {
+          nospawn: true
+        }
+      },
+      scripts: {
+        files: 'app/**/*.js',
+        tasks: ['jshint', 'uglify']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-express');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'less', 'express', 'express-keepalive', 'watch:reload']);
+
 };
